ed50d89163395cb16f6d5be7bd7d4eb6e046848e
VadymBoguslavsky <axixe92@gmail.com>
2018-09-24 16:13:37 +0300

    js

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,19 +5,27 @@ $(function () {
 });
 
 
-var currentTab = 0;
-showTab(currentTab);
-function showTab(n) {
-    var x = document.getElementsByClassName("tab");
-    x[n].style.display = "inline-table";
-}
-function nextPrev(n) {
-    var x = document.getElementsByClassName("tab");
-    x[currentTab].style.display = "none";
-    currentTab = currentTab + n;
-    if (currentTab >= x.length) {
-        document.getElementById("regForm").submit();
-        return false;
+(function () {
+    var currentTab = 0;
+    var nextBtn = document.getElementById("nextBtn")
+    var prevBtn = document.getElementById("prevBtn")
+    nextBtn.addEventListener("click", function(){ nextPrev(1)})
+    prevBtn.addEventListener("click", function () { nextPrev(-1) })
+
+    function showTab(n) {
+        var x = document.getElementsByClassName("tab");
+        x[n].style.display = "block";
+    }
+    
+    function nextPrev(n) {
+        var x = document.getElementsByClassName("tab");
+        x[currentTab].style.display = "none";
+        currentTab = currentTab + n;
+        if (currentTab >= x.length) {
+            document.getElementById("regForm").submit();
+            return false;
+        }
+        showTab(currentTab);
     }
     showTab(currentTab);
-}
+}())
